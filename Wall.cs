d01636bcf34d6a712148944df1d90f3066505019
Benjamin Calmerman <a24benca@student.his.se>
2024-10-01 16:26:14 +0200

    stuff is drawn

--- Wall.cs
+++ Wall.cs
@@ -7,14 +7,14 @@ public sealed class Wall : Entity
 {
     public Wall() : base("pacman")
     {
-        
+        sprite.TextureRect = new IntRect(54, 54, 18, 18);
+        sprite.Origin = new Vector2f(9, 9);
     }
 
     public override bool Solid => true;
     public override void Create(Scene scene)
     {
         base.Create(scene);
-        sprite.TextureRect = new IntRect(0, 0, 18, 18);
     }
     public override void Update(Scene scene, float deltaTime) {}
 }
